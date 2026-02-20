b8cf8bdb66f2c1ae56db20b56c32ed834cd1c321
DA-344 <108473820+DA-344@users.noreply.github.com>
2026-02-21 00:16:59 +0100

    chore(gateway): First pass of event router & models

---
+++ relink/gateway/events/raw_models.py
@@ -0,0 +1,124 @@
+"""
+MIT License
+
+Copyright (c) 2019-2025 PythonistaGuild, EvieePy; 2025-present ReWaveLink Development Team.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from __future__ import annotations
+
+import abc
+from typing import TYPE_CHECKING, Any, Generic, TypeVar
+
+import msgspec
+
+if TYPE_CHECKING:
+    from ..schemas import events, receive
+
+T = TypeVar("T", bound=msgspec.Struct, covariant=True)
+
+
+class EventModel(abc.ABC, Generic[T]):
+    """Base class where all events models derive from.
+
+    This is an :class:`abc.ABC` subclass.
+    """
+
+    __repr_attrs__: tuple[str, ...]
+    _underlying: T
+
+    def __init__(self, underlying: T) -> None:
+        self._underlying = underlying
+
+    def __getattr__(self, name: str) -> Any:
+        try:
+            return getattr(self._underlying, name)
+        except AttributeError as exc:
+            raise AttributeError(f"{self.__class__.__name__} does not have an attribute named {name}") from exc
+
+    def __repr__(self) -> str:
+        attrs = ", ".join(f"{a}={getattr(self, a)}" for a in self.__repr_attrs__)
+        return f"<{self.__class__.__name__} {attrs}>"
+
+
+class ReadyEvent(EventModel["receive.ReadyEvent"]):
+    """Represents a ready event."""
+
+    __repr_attrs__ = ("resumed", "session_id")
+
+    resumed: bool
+    """Whether the session was resumed, if this is ``False`` it implies a new connection
+    was created.
+    """
+    session_id: str
+    """The secret session ID."""
+
+
+class PlayerUpdateEvent(EventModel["receive.PlayerUpdateEvent"]):
+    """Represents a player_update event."""
+
+    __repr_attrs__ = ("state", "guild_id")
+
+    state: receive.PlayerState
+    """The state of the player."""
+    guild_id: int
+    """The guild ID of the player."""
+
+
+class StatsEvent(EventModel["receive.StatsEvent"]):
+    """Represents a stats event."""
+
+    __repr_attrs__ = (
+        "players",
+        "playing_players",
+        "uptime",
+        "memory",
+        "cpu",
+        "frame_stats",
+    )
+
+    players: int
+    """The players count attached to the node."""
+    playing_players: int
+    """The players count attached to the node that are playing a track."""
+    uptime: int
+    """The node uptime in milliseconds."""
+    memory: receive.MemoryStats
+    """The memory stats of the node."""
+    cpu: receive.CPUStats
+    """The CPU stats of the node."""
+    frame_stats: receive.FrameStats | None
+    """The frame stats of the node."""
+
+
+class WSCloseEvent(EventModel["receive.WebSocketClosedEvent"]):
+    """Represents a ws_close event."""
+
+    __repr_attr__ = (
+        "code",
+        "reason",
+        "by_remote",
+    )
+
+    code: int
+    """The Discord close event code."""
+    reason: str
+    """The reason why the connection was closed."""
+    by_remote: bool
+    """Whether the closure was made by Discord."""
---
+++ relink/gateway/events/router.py
@@ -0,0 +1,142 @@
+"""
+MIT License
+
+Copyright (c) 2019-2025 PythonistaGuild, EvieePy; 2025-present ReWaveLink Development Team.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from __future__ import annotations
+
+import asyncio
+from collections.abc import Callable
+import sys
+from typing import TYPE_CHECKING, Any, Coroutine, ParamSpec, TypeVar, overload
+
+if TYPE_CHECKING:
+    from relink.gateway.player import Player
+
+if sys.version_info < (3, 14):
+    from asyncio import iscoroutinefunction as iscoro
+else:
+    from inspect import iscoroutinefunction as iscoro
+
+P = ParamSpec("P")
+T = TypeVar("T")
+EventCallback = Callable[P, Coroutine[Any, Any, T]]
+
+
+class EventRouter:
+    """Represents an event router that handles receiving and dispatching events
+    on a :class:`relink.gateway.Player`.
+    """
+
+    _player: Player
+    _event_map: dict[str, list[EventCallback[..., Any]]]
+    _task_set: set[asyncio.Task[Any]]
+
+    def __init__(self, player: Player) -> None:
+        self._player = player
+        self._event_map = {}
+        self._task_set = set()
+
+    @overload
+    def listen(self, func: EventCallback[P, T], /) -> EventCallback[P, T]: ...
+
+    @overload
+    def listen(self, *, event: str) -> Callable[[EventCallback[P, T]], EventCallback[P, T]]: ...
+
+    @overload
+    def listen(self, *, event: None = None) -> Callable[[EventCallback[P, T]], EventCallback[P, T]]: ...
+
+    def listen(self, func: EventCallback[P, T] | None = None, event: str | None = None) -> Any:
+        """Registers a new event listener.
+
+        This is a decorator that can be used in three different ways:
+
+            .. code-block:: python3
+
+                @router.listen
+                async def on_event():
+                    ...
+
+                @router.listen()
+                async def on_event():
+                    ...
+
+                @router.listen("event")
+                async def function():
+                    ...
+
+        Parameters
+        ----------
+        event: :class:`str` | :data:`None`
+            The event to listen to. If this is ``None`` or not set, it extracts the event
+            name from the decorated function.
+        """
+
+        # case 1 -> func is not None: invoked without parenthesis
+        if func is not None:
+            event_name = func.__name__.removeprefix("on_")
+
+            if not iscoro(func):
+                raise TypeError("event callbacks must be coroutines")
+
+            try:
+                self._event_map[event_name].append(func)
+            except KeyError:
+                self._event_map[event_name] = [func]
+
+            return func
+
+        # case 2 & 3 -> name is maybe None: invoked with parenthesis
+        def inner(func: EventCallback[P, T]) -> EventCallback[P, T]:
+            event_name = event
+            if event_name is None:
+                event_name = func.__name__.removeprefix("on_")
+
+            if not iscoro(func):
+                raise TypeError("event callbacks must be coroutines")
+
+            try:
+                self._event_map[event_name].append(func)
+            except KeyError:
+                self._event_map[event_name] = [func]
+            return func
+        return inner
+
+    def _inner_dispatch(self, event: str, coro: Coroutine[Any, Any, Any]) -> None:
+        task = asyncio.create_task(coro, name=f"relink-event-dispatcher:{event}-{id(coro):#x}")
+        self._task_set.add(task)
+        task.add_done_callback(self._task_set.discard)
+
+    def dispatch(self, event: str, *args: Any, **kwargs: Any) -> None:
+        """Dispatches all event listeners for ``event``.
+
+        Parameters
+        ----------
+        event: :class:`str`
+            The event to be dispatched. This must not be prefixed with ``on_``.
+        *args: Any
+            The positional arguments to pass to the event's callbacks.
+        **kwargs: Any
+            The keyword arguments to pass to the event's callbacks.
+        """
+
+        for func in self._event_map.get(event, []):
+            self._inner_dispatch(event, func(*args, **kwargs))
