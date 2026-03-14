2ca12da067bbe728a828ae72c7a42865653799c8
DA-344 <108473820+DA-344@users.noreply.github.com>
2026-03-14 22:36:09 +0100

    chore: add library exports

---
+++ relink/__init__.py
@@ -0,0 +1,13 @@
+"""
+relink
+~~~~~~
+
+An async-prepared, high performance, Lavalink wrapper for discord.py.
+
+:copyright: (c) 2019-2026 PythonistaGuild, EvieePy; 2026-present ReWaveLink Development Team
+:license: MIT
+"""
+
+from . import gateway, rest, models
+from .errors import *
+from ._version import *
