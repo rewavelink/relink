4bf507efd11b90c37a11d651507164ed233d63fa
Soheab <33902984+Soheab@users.noreply.github.com>
2026-04-03 18:10:15 +0200

    chore: define explicit kwargs for Player

--- relink/gateway/player/__init__.py
+++ relink/gateway/player/__init__.py
@@ -1,9 +1,16 @@
 import logging
 import os
-from typing import Any, cast
+from typing import TYPE_CHECKING, Any, cast
 
 from ._base import BasePlayer, PlayerConnectionState
 from ._factory import FrameworkLiteral, PlayerFactory
+from ..enums import QueueMode
+
+if TYPE_CHECKING:
+    from ..node import Node
+    from ...models.settings import AutoPlaySettings, HistorySettings
+
+    from ...models.filters import Filters
 
 __all__ = (
     "Player",
@@ -20,7 +27,17 @@ class Player(BasePlayer):
     A dynamic proxy class for ReLink players.
     """
 
-    def __new__(cls, *args: Any, **kwargs: Any) -> Any:
+    def __new__(
+        cls,
+        *,
+        node: Node | None = None,
+        queue_mode: QueueMode = QueueMode.NORMAL,
+        autoplay_settings: AutoPlaySettings | None = None,
+        history_settings: HistorySettings | None = None,
+        volume: int | None = None,
+        paused: bool | None = None,
+        filters: Filters | None = None,
+    ) -> Any:
         env_framework = os.getenv("RELINK_FRAMEWORK", "discord.py")
         framework = cast(FrameworkLiteral, env_framework)
 
@@ -28,7 +45,15 @@ class Player(BasePlayer):
         actual_class = factory.get_player(framework)
 
         instance = object.__new__(actual_class)
-        instance.__init__(*args, **kwargs)
+        instance.__init__(
+            node=node,
+            queue_mode=queue_mode,
+            autoplay_settings=autoplay_settings,
+            history_settings=history_settings,
+            volume=volume,
+            paused=paused,
+            filters=filters,
+        )
 
         return instance
 
